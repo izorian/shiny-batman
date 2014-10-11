26199e326111e7763f488b6c05ba610d03a3ff45
Nicolas Cherel <nicolas.cherel@telecom-paristech.fr>
2014-10-11 11:52:28 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/**
+ * @author
+ */
+
+$('nav a').on('click', function(e) {
+	e.preventDefault();
+	var url = this.href;
+	
+	$('nav a.current').removeClass('current');
+	$(this).addClass('current');
+	
+	$('#container').remove();
+	$('section').load(url + ' #container').hide().fadeIn('slow');
+});
